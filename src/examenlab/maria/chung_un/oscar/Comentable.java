637bb7232bceb62ad449354554a38423c9b6ad23
Maria-Rodriguez01 <rodriguez_maria@unitec.edu>
2025-02-21 13:42:16 -0600

    interfaz comentable

---
+++ src/examenlab/maria/chung_un/oscar/Comentable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package examenlab.maria.chung_un.oscar;
+
+/**
+ *
+ * @author Maria Gabriela
+ */
+public interface Comentable {
+    boolean addComment();
+}
