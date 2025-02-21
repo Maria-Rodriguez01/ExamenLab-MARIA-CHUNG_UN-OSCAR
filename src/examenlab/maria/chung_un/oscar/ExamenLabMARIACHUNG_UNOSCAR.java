16fa89bb527f021c72937c8a652d91d574593072
Maria-Rodriguez01 <rodriguez_maria@unitec.edu>
2025-02-21 13:18:07 -0600

    primer

---
+++ src/examenlab/maria/chung_un/oscar/ExamenLabMARIACHUNG_UNOSCAR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenlab.maria.chung_un.oscar;
+
+/**
+ *
+ * @author Maria Gabriela
+ */
+public class ExamenLabMARIACHUNG_UNOSCAR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
